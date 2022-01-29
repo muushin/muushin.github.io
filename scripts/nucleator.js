4f769e8da64af081a8f5a5280f23bacbfa0c96c3
muushin <andrew@journeyman.pw>
2022-01-29 14:56:10 -0600

    use sha256 of idea text instead of uuid for localstorage key

--- scripts/nucleator.js
+++ scripts/nucleator.js
@@ -1,26 +1,48 @@
-function keep_idea() {
+document.onload = introduction();
+
+function introduction() {
+  console.log('  Welcome to Nucleator\n  Logging my activity here...');
+}
+
+async function keep_idea() {
+  console.log("KEEP button pressed");
   var tbox = document.getElementById('scratchpad');
-  var key = get_uuid();
-  localStorage.setItem(key, tbox.value)
+  if (tbox.value != '') {
+   var key = await get_digest(tbox.value);
+   localStorage.setItem(key, tbox.value);
+   console.log("saving current scratchpad text");
+  }
+  console.log("---");
 }
 
 function clear_scratchpad() {
+  console.log("CLEAR button pressed");
   var tbox = document.getElementById('scratchpad');
   tbox.value = "";
+  console.log("clearing scratchpad\n---");
 }
 
 function nucleate() {
+  console.log("NUCLEATE button pressed")
   var tbox = document.getElementById('scratchpad');
   var key = get_random_storage_key();
   tbox.value = localStorage.getItem(key);
+  console.log("loading key :\n" + key);
+  console.log('---');
 }
 
-function get_uuid() {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16));
+async function get_digest(idea) {
+  console.log("getting hex hash of scratchpad text");
+  const data = new TextEncoder().encode(idea);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  console.log('hash: \n' + hashHex);
+  return hashHex;
 }
 
 function get_random_storage_key() {
+  console.log('getting random storage key');
   var I = Math.floor(Math.random() * (localStorage.length));
-  return localStorage.key(I)
+  return localStorage.key(I);
 }
